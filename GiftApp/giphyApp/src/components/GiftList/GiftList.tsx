5e4a627802509758666aa90f898443fd2d3b16db
Martin Cicala <tu_correo@example.com>
2024-10-08 00:24:43 -0300

    Agrego GiphyApp

---
+++ GiftApp/giphyApp/src/components/GiftList/GiftList.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { CardGift } from "../ui/CardGift/CardGift";
+
+interface IGift{
+    urlGift: string,
+    title: string
+}
+interface IPropsGiftList{
+    gift: IGift[];
+}
+
+export const GiftList: FC<IPropsGiftList> = ({gift}) => {
+    return (
+    <div
+    style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 0.6fr)",
+        gap: "2vh",
+    }}>
+        {gift.map((el, i)=>(
+            <CardGift gift={el} key={i}/>
+        ))}
+    </div>
+    );
+};
